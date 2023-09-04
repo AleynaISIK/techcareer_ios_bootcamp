c97db679f7d7a9a4b132680640987ba547b78c54
Aleyna-ISIK <aleynaisik100@gmail.com>
2023-09-04 03:38:01 +0300

    Initial commit

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+var sehir = "Muğla"
+print("Şehir              : \(sehir)")
+
+let ulke = "Türkiye"
+print("Ülke               : \(ulke)")
+
+var telefon = 05530317472
+print("Telefon            : \(telefon)")
+
+let postaKodu = 48000
+print("Posta Kodu         : \(postaKodu)")
+
+var email = "aleynaisik100@gmail.com"
+print("E-mail             : \(email)")
+
+var meslek = "Ios Developer"
+print("Meslek             : \(meslek)")
+
+let stokMiktari = 200
+print("Stok Miktarı       : \(stokMiktari)")
+
+let musteriAdi = "Alara"
+print("Müşteri Adı        : \(musteriAdi)")
+
+var bakiye = 5000
+print("Bakiye             : \(bakiye) ₺")
+
+let dogumGunu = "05/11/1998"
+print("Doğum Günü         : \(dogumGunu)")
+
+var maas = 40.0000
+print("Maaş               : \(maas) ₺")
+
+var medeniDurum = "Bekar"
+print("Medeni Durum       : \(medeniDurum)")
+
+var urunYorum = "Mükemmel bir ürün, tavsiye ederim"
+print("Ürün Yorumu        : \(urunYorum)")
+
+var odemeTarihi = "07/05/2024"
+print("Ödeme Tarihi       : \(odemeTarihi)")
+
+var odeme = "Nakit"
+print("Ödeme              : \(odeme)")
+
+var siparisAdeti = 40
+print("Sipariş Adeti      : \(siparisAdeti)")
+
+let arabaModeli = 2023
+print("Araba Modeli       : \(arabaModeli)")
+
+var kitapAdi = "On Küçük Zenci"
+print("Kitap Adı          : \(kitapAdi)")
+
+var yayinlamaTarihi = "15/05/2000"
+print("Yayınlama Tarihi   : \(yayinlamaTarihi)")
+
+var indirimMiktari = 50
+print("İndirim Miktarı    : % \(indirimMiktari)")
+
+var odaSayisi = 100
+print("Oda Sayısı         : \(odaSayisi)")
+
+let enlem = "37° 12'"
+print("Muğla Enlemi       : \(enlem) Kuzey enlemleri")
+
+let boylam = " 28° 21'"
+print("Muğla Boylamı      : \(boylam) Doğu boylamları")
+
+var urunAdi = "Telefon"
+print("Ürün Adı           : \(urunAdi)")
+
+var yemekFiyati = 485
+print("Yemek Fiyatı       : \(yemekFiyati)")
+
+var marka = "Gucci"
+print("Marka              : \(marka)")
+
+var muzikAdi = "Wonderwall"
+print("Müzik Adı          : \(muzikAdi)")
+
+var videoaSuresi = 20
+print("Video Süresi       : \(videoaSuresi) dakika")
+
+var urunPuani = 5
+print("Ürün Puanı (1-5)   : \(urunPuani)")
+
+var resimAdi = "Grafiti"
+print("Resim Adı          : \(resimAdi)")
+
+var dosyaFormati = "jpg"
+print("Dosya Formatı      : \(dosyaFormati)")
+
+var renk = "Mor"
+print("Renk               : \(renk)")
+
+var renkKodu = "B281EB"
+print("Renk Kodu          : \(renkKodu)")
+
+var telefonModeli = "iPhone 14"
+print("Telefon Modeli     : \(odeme)")
+
+var ekranBoyutu = 6.1
+print("Ekran Boyutu       : \(ekranBoyutu) inç")
+
+var agirlik = 172
+print("Ağırlık            : \(agirlik) gram")
+
+var ulusalGun = "29 Ekim"
+print("Ulusal Gün         : \(ulusalGun)")
+
+var tatilGunu = "Cumartesi"
+print("Tatil Günü         : \(tatilGunu)")
+
+var rezervasyonTarihi = "09/09/2023"
+print("Rezervasyon Tarihi : \(rezervasyonTarihi)")
+
+var sokakAdi = "Çiçek"
+print("Sokak Adı          : \(sokakAdi)")
+
+var otobusHatti = "1-1"
+print("Otobüs Hattı       : \(otobusHatti)")
+
+var kalanDakika = 1020
+print("Kalan Dakika       : \(kalanDakika) dk")
+
+var takipKodu = 482418
+print("Takip Kodu         : \(takipKodu)")
+
+var kupon_suresi = 20
+print("Kupon Süresi       : \(kupon_suresi) gün")
+
+var kupon_kodu = "AX8JDI8"
+print("Kupon Kodu         : \(kupon_kodu)")
+    
+var fatura_adresi = "Yağmur Mahallesi 100 sokak"
+print("Fatura Adresi      : \(fatura_adresi)")
---
+++ degiskenOlusturma.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+print("Merhaba Dünya")
+
+var ogrenciAdi = "Aleyna"
+var ogrenciYas = 24
+var ogrenciBoy = 1.61
+var ogrenciBasharf = "A"
+var ogrenciDevamEdiyorMu = true
+
+print(ogrenciAdi)
+print(ogrenciBoy)
+print(ogrenciYas)
+print(ogrenciBasharf)
+print(ogrenciDevamEdiyorMu)
+
+print( "Adı : ",ogrenciAdi)
+
+
+var urun_id: Int = 3416
+var urun_adi: String = "Kol Saati"
+var urun_adet: Int = 100
+var urun_fiyat: Double = 149.99
+var urun_tedarikci: String = "Rolex"
+
+print("Ürün id        : \(urun_id)")
+print("Ürün Adı       : \(urun_adi)")
+print("Ürün Adet      : \(urun_adet)")
+print("Ürün fiyat     : \(urun_fiyat) ₺")
+print("Ürün tedarikçi : \(urun_tedarikci)")
+
+//Comment Line
+//Constant - Sabit
+//Swift(let) - Kotlin(val) - Dart,Java(final) -const
+
+var sayi = 10
+print(sayi)
+sayi = 99
+print(sayi)
+
+let numara = 20
+print(numara)
+
+//tür dönüşümü - type casting
+
+//sayısal --> sayısal
+var i = 43
+var d = 56.78
+
+var sonuc1 = Double(i)
+var sonuc2 = Int(d)
+
+print(sonuc1 , sonuc2)
+
+//Sayısal --> Metin
+
+var sonuc3 = String(i)
+var sonuc4 = String(d)
+print(sonuc3, sonuc4)
+
+// Metin --> Sayısal
+
+var yazi  = "34t"
+
+if let sonuc5 = Int(yazi){ //optinal binding if let yapısı
+  print(sonuc5)
+
+}else{
+  print("Dönüşüm hatası")
+}
