9c1baed7ced228e9d883f4ad0142ce9cad501f16
Aleyna-ISIK <aleynaisik100@gmail.com>
2023-09-13 19:37:11 +0300

    edit

--- TasarimCalismasi/TasarimCalismasi/ViewController.swift
+++ TasarimCalismasi/TasarimCalismasi/ViewController.swift
@@ -20,8 +20,11 @@ class ViewController: UIViewController {
     navigationController?.navigationBar.barStyle = .black //üstteki saat rengi beyaz yapma ayarı
     //Cmd+shift+a similatörde gece gündüz değiştirme
     
+//    gezinme çubuğunun standart (normal) görünümünü yapılandırır. Standart görünüm, gezinme çubuğunun varsayılan durumunu temsil eder.
     navigationController?.navigationBar.standardAppearance = appereance
+//    gezinme çubuğunun kompakt (dar) görünümünü yapılandırır. Kompakt görünüm, ekran boyutu veya diğer faktörler nedeniyle gezinme çubuğunun daha sınırlı bir alanı olduğu durumlar için kullanılır.
     navigationController?.navigationBar.compactAppearance = appereance
+//    gezinme çubuğunun kaydırma kenarı görünümünü yapılandırır. Bu görünüm, özellikle ekranın üstünden aşağı kaydırıldığında gezinme çubuğunun nasıl görüneceğini belirler.
     navigationController?.navigationBar.scrollEdgeAppearance = appereance
     
   }
